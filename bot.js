cac19cd6b53bb6088a03c9649443082e141452fc
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-02 09:44:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ console.log("Welcome Again !");
   member.createDM().then(function (channel) {
   return channel.send(` 
 **
-[ Welcome To Bond ] ,
-ارحبو تراحيب المطرر , تعالو نستانس انتم واخوياكم .. 
-ننتظركـــم ، 
-[ https://discord.gg/pZVzBf4 ] ,
+[ Welcome To Royal Games. ] ,
+صوتولي على المتسآابقق رقم 
+: 15 : وانشرهه لاخوياكممم , 
+[ https://discord.gg/9XDCb65 ] ,
 [ https://images-ext-1.discordapp.net/external/5j2inxAmKsGZRr8oGuoeKCynVBzqItDw-eSyBA4mHpM/https/media3.giphy.com/media/3o6Zt6zRQw8yStXfxe/giphy.gif?width=432&height=324 ]
 الدعووة خاصة لك ي قلبي ... [ ${member}  ]
 **`)
@@ -22,19 +22,17 @@ client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
 **
-[ Welcome To Bond ] ,
-ارحبو تراحيب المطرر , تعالو نستانس انتم واخوياكم .. 
-ننتظركـــم ، 
-[ https://discord.gg/pZVzBf4 ] ,
+[ Welcome To Royal Games. ] ,
+صوتولي على المتسآابقق رقم 
+: 15 : وانشرهه لاخوياكممم , 
+[ https://discord.gg/9XDCb65 ] ,
 [ https://images-ext-1.discordapp.net/external/5j2inxAmKsGZRr8oGuoeKCynVBzqItDw-eSyBA4mHpM/https/media3.giphy.com/media/3o6Zt6zRQw8yStXfxe/giphy.gif?width=432&height=324 ]
 الدعووة خاصة لك ي قلبي ... [ ${member}  ]
 **`) 
 }).catch(console.error)
 })
 
-    client.on('ready', () => {
-     client.user.setActivity("- Bond . By : SoM .",{type: 'Listening'});
-
-});
 
 client.login(process.env.BOT_TOKEN);
+
+
